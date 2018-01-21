2dbfa4bfe3e1ea4af146ac6447120c272c8c0057
Israel Baurel Vázquez Morales <galathisrael@yahoo.com.mx>
2018-01-21 03:50:23 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AlertModule } from 'ngx-bootstrap/alert';
+
+import { IconsModule } from './icons/icons.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IconsModule,
+    BsDropdownModule.forRoot(),
+    AlertModule.forRoot(),
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/icons/icons.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {
+  IconTag,
+  IconSearch,
+  IconShare,
+  IconMaximize2,
+  IconMinus,
+  IconX
+} from 'angular-feather';
+
+@NgModule({
+  exports: [
+    IconTag,
+    IconSearch,
+    IconShare,
+    IconMaximize2,
+    IconMinus,
+    IconX,
+  ]
+})
+export class IconsModule { }
